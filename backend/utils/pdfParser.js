8e226544d8bc6fc948740f1f4ee2fc84fc6cf919
yatharth09 <yatharth25thapa@gmail.com>
2025-12-18 18:31:34 +0530

    pdf-parsing

--- backend/utils/pdfParser.js
+++ backend/utils/pdfParser.js
@@ -1,18 +1,18 @@
-import fs from 'fs/promises';
-import { PDFParse } from 'pdf-parse';
+import fs from "fs/promises";
+import pdf from "pdf-parse";
 
 export const extractTextFromPDF = async (filePath) => {
-    try {
-        const dataBuffer = await fs.readFile(filePath);
-        const parser = new PDFParse(new Uint8Array(dataBuffer));
-        const data = await parser.getText()
+  try {
+    const dataBuffer = await fs.readFile(filePath);
 
-        return {
-            text: data.text,
-            numPages: data.numpages,
-            info: data.info,
-        };
-    } catch (error) {
-        throw new Error('Error parsing PDF: ' + error.message);
-    }
-}
+    const data = await pdf(dataBuffer);
+
+    return {
+      text: data.text,
+      numPages: data.numpages,
+      info: data.info,
+    };
+  } catch (error) {
+    throw new Error("Error parsing PDF: " + error.message);
+  }
+};
